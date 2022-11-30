7d201548a211257c96b20f5667065bc39dc474dd
Thien Tran <webmaster3t@gmail.com>
2022-11-30 20:46:39 +0700

    remove unuse code

--- dnd-ui/src/editors/ctflowEditor.ts
+++ dnd-ui/src/editors/ctflowEditor.ts
@@ -90,58 +90,6 @@ export class CtFlowEditorProvider implements vscode.CustomTextEditorProvider {
 		updateWebview();
 	}
 
-	/**
-	 * Get the static html used for the editor webviews.
-	 */
-	// private getHtmlForWebview(webview: vscode.Webview): string {
-	// 	// Local path to script and css for the webview
-	// 	const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
-	// 		this.context.extensionUri, 'media', 'catScratch.js'));
-
-	// 	const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(
-	// 		this.context.extensionUri, 'media', 'reset.css'));
-
-	// 	const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(
-	// 		this.context.extensionUri, 'media', 'vscode.css'));
-
-	// 	const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(
-	// 		this.context.extensionUri, 'media', 'catScratch.css'));
-
-	// 	// Use a nonce to whitelist which scripts can be run
-	// 	const nonce = getNonce();
-
-	// 	return /* html */`
-	// 		<!DOCTYPE html>
-	// 		<html lang="en">
-	// 		<head>
-	// 			<meta charset="UTF-8">
-
-	// 			<!--
-	// 			Use a content security policy to only allow loading images from https or from our extension directory,
-	// 			and only allow scripts that have a specific nonce.
-	// 			-->
-	// 			<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-
-	// 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	// 			<link href="${styleResetUri}" rel="stylesheet" />
-	// 			<link href="${styleVSCodeUri}" rel="stylesheet" />
-	// 			<link href="${styleMainUri}" rel="stylesheet" />
-
-	// 			<title>Cat Scratch</title>
-	// 		</head>
-	// 		<body>
-	// 			<div class="notes">
-	// 				<div class="add-button">
-	// 					<button>Scratch!</button>
-	// 				</div>
-	// 			</div>
-
-	// 			<script nonce="${nonce}" src="${scriptUri}"></script>
-	// 		</body>
-	// 		</html>`;
-	// }
-
 	private getHtmlForWebview(webview: vscode.Webview, extensionUri: Uri): string {
 		// The CSS file from the React build output
 		const stylesUri = getUri(webview, extensionUri, ["webview-ui", "build", "assets", "index.css"]);
