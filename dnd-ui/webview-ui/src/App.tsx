c8b3e69ac5833d2fce1d6ab25e6710e8ad52873e
ghun131 <hungdh131@gmail.com>
2022-12-10 10:53:24 +0700

    update App

--- dnd-ui/webview-ui/src/App.tsx
+++ dnd-ui/webview-ui/src/App.tsx
@@ -1,27 +1,43 @@
+import { useState } from "react";
+import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
+import createFastContext from "./context/createFastContext";
 
-import { useState } from 'react';
-import { VSCodeButton } from '@vscode/webview-ui-toolkit/react';
+import Flow from "./Flow";
+import { vscode } from "./utilities/vscode";
 
-import Flow from './Flow';
-import { vscode } from './utilities/vscode';
+import "./App.css";
+import { TextInput } from "./models/TextInput";
 
-import './App.css';
+const { Provider, useStore } = createFastContext({
+  nodes: {},
+  edges: {},
+});
 
 function App() {
-  const [count, setCount] = useState(0);
-  function handleHowdyClick() {
-    vscode.postMessage({
-      command: "hello",
-      text: "Hey there partner! 🤠",
-    });
-  }
+  // function handleHowdyClick() {
+  //   vscode.postMessage({
+  //     command: "hello",
+  //     text: "Hey there partner! 🤠",
+  //   });
+  // }
+  const textInputNode = new TextInput({
+    id: "1",
+    type: "text",
+    data: { a: 1 },
+    position: { x: 111, y: 222 },
+    inPorts: { email: "hungdh131@gmail.com" },
+    outPorts: {},
+  });
+  console.log("textInputNode", textInputNode);
 
   return (
-    <main>
-      <div className="App">
-        <Flow />
-      </div>
-    </main>
+    <Provider>
+      <main>
+        <div className="App">
+          <Flow />
+        </div>
+      </main>
+    </Provider>
   );
 }
 
