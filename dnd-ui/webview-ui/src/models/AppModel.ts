04d32bcafc624df3de979c85fe6898b9e13b5a15
ghun131 <hungdh131@gmail.com>
2022-12-10 10:53:48 +0700

    webview context

--- dnd-ui/webview-ui/src/models/AppModel.ts
+++ dnd-ui/webview-ui/src/models/AppModel.ts
@@ -1,13 +1,37 @@
-export class AppModel {
-  constructor(options?: any) {}
-  id: string | undefined;
+interface IOptions {
+  id?: string;
   type: string | undefined;
   data: any;
-  position: { x: number; y: number; } | undefined;
+  position: { x: number; y: number } | undefined;
   inPorts: any;
   outPorts: any;
-  icon: string | undefined;
-  description: string | undefined;
-  componentName: string | null | undefined;
-  outputQ: any[] | undefined;
+  icon?: string | undefined;
+  description?: string | undefined;
+  componentName?: string | null | undefined;
+  outputQ?: any[] | undefined;
+}
+
+export class AppModel {
+  id: string;
+  type: string;
+  data: any;
+  position: { x: number; y: number };
+  inPorts: any;
+  outPorts: any;
+  icon?: string;
+  description?: string;
+  componentName?: string | null;
+  outputQ?: any[];
+  constructor(options: IOptions) {
+    this.id = options.id || 'string';
+    this.type = options.type || 'notype';
+    this.data = options.data;
+    this.position = options.position || { x: 0, y: 0};
+    this.inPorts = options.inPorts;
+    this.outPorts = options.outPorts;
+    this.icon = options.icon;
+    this.description = options.description || 'description';
+    this.componentName = options.componentName || 'compName';
+    this.outputQ = options.outputQ || ['outputQ'];
+  }
 }
