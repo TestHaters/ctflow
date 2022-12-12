bf729730fef05859bab88959c0753548cbb258f4
Thien Tran <webmaster3t@gmail.com>
2022-12-12 20:53:49 +0700

    reload state when file change

--- dnd-ui/webview-ui/src/dataLoader.ts
+++ dnd-ui/webview-ui/src/dataLoader.ts
@@ -40,6 +40,8 @@ export class DataLoader {
   onFileUpdate(event: any) {
     console.log("FILE UPDATE", event)
     this.parseYaml(event.data.text)
+    const [_, setGlobalStore] = useStore((store) => store.nodes);
+    setGlobalStore({ nodes: this.nodes, edges: this.edges });
   }
 
   // when flow is updated, push a temporary version to ctflowEditor
