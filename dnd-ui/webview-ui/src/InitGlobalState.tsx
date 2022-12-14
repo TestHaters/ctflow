2c2c2d0320496a6b4206c03829a8c9bb9cec38d0
ghun131 <hungdh131@gmail.com>
2022-12-15 00:25:25 +0700

    ref: only setStore once

--- dnd-ui/webview-ui/src/InitGlobalState.tsx
+++ dnd-ui/webview-ui/src/InitGlobalState.tsx
@@ -1,28 +1,25 @@
-import { useEffect } from 'react';
-import { useStore } from "./context/store"
-import { Router } from './router';
-import { DataLoader } from './dataLoader';
-import { vscode } from './utilities/vscode'
-
+import { useEffect } from "react";
+import { useStore } from "./context/store";
+import { Router } from "./router";
+import { DataLoader } from "./dataLoader";
+import { vscode } from "./utilities/vscode";
 
 export default function InitGlobalState({ children }: { children: any }) {
-	const [router, setRouter] = useStore((store) => store.router);
-	const [dataLoader, setDataLoader] = useStore((store) => store.dataLoader)
-	useEffect(() => {
-		const router = new Router();
-		const dataLoader = new DataLoader();
+  const [store, setStore] = useStore((store) => store);
+  useEffect(() => {
+    const router = new Router();
+    const dataLoader = new DataLoader();
 
-		dataLoader.subscribe(router)
-		window.router = router
-		window.dataLoader = dataLoader
+    dataLoader.subscribe(router);
+    window.router = router;
+    window.dataLoader = dataLoader;
 
-		// TODO: Fix this hack
-		// After router & dataLoader are loaded, request the document data again
-		vscode.postMessage({ type: 'fetchDocumentData', })
+    // TODO: Fix this hack
+    // After router & dataLoader are loaded, request the document data again
+    vscode.postMessage({ type: "fetchDocumentData" });
 
-		setRouter({ router: router })
-		setDataLoader({ dataLoader: dataLoader })
-	}, [])
+    setStore({ router, dataLoader, nodes: dataLoader.nodes, edges: dataLoader.edges });
+  }, []);
 
-	return <div>{children}</div>
+  return <div>{children}</div>;
 }
