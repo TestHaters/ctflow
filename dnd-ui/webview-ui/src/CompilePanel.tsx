03fa017861228708b9dd842cc6fb8fd7624ce23b
Du <hoangminhdu2000@gmail.com>
2022-12-16 14:30:01 +0700

    update style save button

--- dnd-ui/webview-ui/src/CompilePanel.tsx
+++ dnd-ui/webview-ui/src/CompilePanel.tsx
@@ -8,11 +8,11 @@ function CompilePanel({ onClick }) {
   // TODO: loading state when onClick running
   return (
     <Panel
-      className="rounded !text-white font-semibold py-2 px-5 bg-green-500	"
+      className="rounded !text-white font-semibold py-2 px-5 bg-blue-500"
       position="top-left"
       // style={{ top: 50 }}
       onClick={onClick}>
-      Compile{" "}
+      Compile
       <span className="ml-1">
         <i className="fa-solid fa-play"></i>
       </span>
--- dnd-ui/webview-ui/src/SavePanel.tsx
+++ dnd-ui/webview-ui/src/SavePanel.tsx
@@ -5,11 +5,14 @@ function SavePanel({ onClick }) {
   // TODO: loading state when onClick running
   return (
     <Panel
-      className="rounded !text-white font-semibold py-2 px-5 bg-red-500	"
+      className="rounded !text-white font-semibold py-2 px-5 bg-green-500	"
       position="top-right"
       style={{ right: 30 }}
       onClick={onClick}>
-      Save{" "}
+      <span className="mr-1">
+        <i className="fa-solid fa-floppy-disk"></i>
+      </span>
+      Save
     </Panel>
   );
 }
