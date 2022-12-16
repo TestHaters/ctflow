349c03e2344d59d20233b2f3c8c77aaddb638681
ghun131 <hungdh131@gmail.com>
2022-12-16 09:46:21 +0700

    change style for compile button

--- dnd-ui/webview-ui/src/CompilePanel.tsx
+++ dnd-ui/webview-ui/src/CompilePanel.tsx
@@ -5,12 +5,15 @@ import { RFNode } from "./nodeFactory";
 import { useOnClickOutside } from "./useClickOutside";
 
 function CompilePanel({ onClick }) {
-
-	return (
-		<Panel position="top-left" style={{ top: 100 }} onClick={onClick}>
-			Compile
-		</Panel>
-	);
+  return (
+    <Panel
+      className="rounded !text-black font-semibold py-2 px-5"
+      position="top-left"
+      style={{ top: 50 }}
+      onClick={onClick}>
+      Compile
+    </Panel>
+  );
 }
 
 export default memo(CompilePanel);
