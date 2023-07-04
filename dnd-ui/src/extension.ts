b9f26a0b85a071009dd540b129211a82963c9f25
Thien Tran <webmaster3t@gmail.com>
2023-07-04 16:12:27 +0700

    update extension information

--- dnd-ui/src/extension.ts
+++ dnd-ui/src/extension.ts
@@ -4,12 +4,12 @@ import { CtFlowEditorProvider } from "./editors/ctflowEditor"
 
 export function activate(context: ExtensionContext) {
   // Create the show hello world command
-  const showHelloWorldCommand = commands.registerCommand("hello-world.showHelloWorld", () => {
-    HelloWorldPanel.render(context.extensionUri);
-  });
+  // const showHelloWorldCommand = commands.registerCommand("hello-world.showHelloWorld", () => {
+  //   HelloWorldPanel.render(context.extensionUri);
+  // });
 
   // Add command to the extension context
-  context.subscriptions.push(showHelloWorldCommand);
+  // context.subscriptions.push(showHelloWorldCommand);
   // Register our custom editor providers
   context.subscriptions.push(CtFlowEditorProvider.register(context))
 }
